dcd0c28821209d3fdfb72263eedd15b6d3245221
Houjun Liu <hliu@shabang.cf>
2021-06-16 22:47:26 -0700

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useEffect, useState } from "react";
+
+const MAX_WIDTH=70 // NUM_COLS
+const MAX_HEIGHT=70 // NUM_ROWS
+
+// The grid is
+// ROW-MAJOR (each index is [row][column])
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let grid = useRef(new Array(MAX_HEIGHT));
+    let [ready, setReady] = useState(false);
+
+    useEffect(()=>{
+        // Assemble grid. A.k.a. "bad 2d array"
+        [...Array(MAX_HEIGHT).keys()].forEach((i) => {
+            grid.current[i] = new Array(MAX_WIDTH);
+        });
+        setReady(true);
+    }, [])
+
+    return (
+        <div className="App">
+            {ready?
+            <div className="container" 
+                style={{
+                    gridTemplateColumns: `repeat(${MAX_WIDTH}, auto)`,
+                    gridTemplateRows: `repeat(${MAX_HEIGHT}, auto)`,
+                }}>
+                {[...Array(MAX_HEIGHT).keys()].map((i) => {
+                    return ([...Array(MAX_WIDTH).keys()].map((j) => {
+                        return (
+                            <div className="cell" key={`cell-${i}_${j}`} id={`cell-${i}_${j}`} style={{
+                                    background: grid.current[i][j] ? "#434d5f" : "inherit"
+                                }}>
+                                &nbsp;
+                            </div>
+                        )
+                    }))
+                })}
+            </div>:<div>Spinnin...</div>}
+        </div>
+    );
 }
 
 export default App;
