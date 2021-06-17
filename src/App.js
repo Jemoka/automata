f39c93aa628cc252615f6f2b07727d159cde1268
Houjun Liu <hliu@shabang.cf>
2021-06-16 23:04:42 -0700

    grid clickable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import "./App.css";
-import { useRef, useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 
-const MAX_WIDTH=70 // NUM_COLS
-const MAX_HEIGHT=70 // NUM_ROWS
+const MAX_WIDTH=80 // NUM_COLS
+const MAX_HEIGHT=80 // NUM_ROWS
 
 // The grid is
 // ROW-MAJOR (each index is [row][column])
 
 function App() {
-    let grid = useRef(new Array(MAX_HEIGHT));
+    let [grid, setGrid] = useState((new Array(MAX_HEIGHT)).fill());
     let [ready, setReady] = useState(false);
+    const [_, forceUpdate] = useReducer(x => x + 1, 0);
 
     useEffect(()=>{
         // Assemble grid. A.k.a. "bad 2d array"
-        [...Array(MAX_HEIGHT).keys()].forEach((i) => {
-            grid.current[i] = new Array(MAX_WIDTH);
+        grid.forEach((_, i) => {
+            grid[i] = (new Array(MAX_WIDTH)).fill();
         });
         setReady(true);
-    }, [])
+        setGrid(grid);
+    }, [grid])
 
     return (
         <div className="App">
@@ -27,11 +29,17 @@ function App() {
                     gridTemplateColumns: `repeat(${MAX_WIDTH}, auto)`,
                     gridTemplateRows: `repeat(${MAX_HEIGHT}, auto)`,
                 }}>
-                {[...Array(MAX_HEIGHT).keys()].map((i) => {
-                    return ([...Array(MAX_WIDTH).keys()].map((j) => {
+                {grid.map((v, i) => {
+                    return (v.map((u, j) => {
                         return (
-                            <div className="cell" key={`cell-${i}_${j}`} id={`cell-${i}_${j}`} style={{
-                                    background: grid.current[i][j] ? "#434d5f" : "inherit"
+                            <div className="cell" key={`cell-${i}_${j}_${u?"f":"u"}`} id={`cell-${i}_${j}_${u?"f":"u"}`} style={{
+                                    background: u ? "#434d5f" : "inherit"
+                                }}
+                                onClick={()=> {
+                                    grid[i][j]=!grid[i][j]
+                                    setGrid(grid);
+                                    forceUpdate();
+                                    // https://stackoverflow.com/questions/46240647/react-how-to-force-a-function-component-to-render
                                 }}>
                                 &nbsp;
                             </div>
